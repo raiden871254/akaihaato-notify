49a9ef360ae31d24f38e172e00c6de022539c7ef
raiden871254 <65803790+raiden871254@users.noreply.github.com>
2021-03-06 17:50:20 +0900

    Add files via upload

---
+++ dics.py
@@ -0,0 +1,22 @@
+#botが見るVTuberのリスト
+members = ['赤井はあと']
+
+#YouTube Ch の Channel ID の辞書
+#赤井はあとさんのチャンネルのURLは，https://www.youtube.com/channel/UCQ0UDLQCjY0rmuxCDE38FGg
+#channel/　以降の部分を書く．
+YT_ID_dic = {'赤井はあと':'UC1CfXB_kRs3C-zaeTG3oGyg',\
+            }
+
+#TwitterのURL
+#mobile番のURLにすること
+tw_url_dic = {'赤井はあと':'https://mobile.twitter.com/akaihaato',\
+             }
+
+#取得したtwitter idの辞書
+tw_id_dic = {'赤井はあと' : 998336069992001537,\
+            }
+
+#https://bitly.com で短縮したチャンネルURL
+#https://www.youtube.com./channel/***********
+bitly_yt_dic = {'赤井はあと':'https://bit.ly/2vSwfee',\
+               }
